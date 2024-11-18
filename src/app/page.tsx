a5e8adb6bbeb7733f981aefacfa99a0c6a915b26
unknown <matheus.lage@icloud.com>
2024-11-18 19:01:28 -0300

    Update deployment configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import AwsQuizApp from '@/components/aws-quiz-app'
 
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-50 py-8">
-      <AwsQuizApp />
-    </main>
-  )
+  return <AwsQuizApp />
 }
