73aa7b00e489e87e35962365229c9bd69c7b579f
unknown <matheus.lage@icloud.com>
2024-11-20 08:26:29 -0300

    Updates

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import AwsQuizApp from '@/components/aws-quiz-app'
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-50 py-8">
-      <AwsQuizApp />
-    </main>
-  )
-}
